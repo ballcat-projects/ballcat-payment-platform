c27c6ec07ead3797fc1ad2c782833f56092b2aef
聆听 <sunlisten.gzm@gmail.com>
2021-06-07 22:12:33 +0800

    :sparkles: 添加虚拟地址类

---
+++ payment-biz/src/main/java/live/lingting/service/VirtualAddressService.java
@@ -0,0 +1,11 @@
+package live.lingting.service;
+
+import com.hccake.extend.mybatis.plus.service.ExtendService;
+import live.lingting.entity.VirtualAddress;
+
+/**
+ * @author lingting 2021/6/7 15:43
+ */
+public interface VirtualAddressService extends ExtendService<VirtualAddress> {
+
+}
