cbcade1f13b3a00664852590cfab68a54b00b872
聆听 <sunlisten.gzm@gmail.com>
2021-06-18 10:24:41 +0800

    :sparkles: 所有超时时间配置值从数据库获取.

--- payment-api/src/main/java/live/lingting/api/thread/VirtualValidThread.java
+++ payment-api/src/main/java/live/lingting/api/thread/VirtualValidThread.java
@@ -8,16 +8,15 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import live.lingting.ApiConfig;
 import live.lingting.api.manager.VirtualManager;
 import live.lingting.api.properties.ApiProperties;
 import live.lingting.entity.Pay;
 import live.lingting.sdk.enums.Currency;
 import live.lingting.sdk.enums.PayStatus;
 import live.lingting.service.PayService;
-import live.lingting.util.SpringUtils;
 import live.lingting.virtual.VirtualHandler;
 import live.lingting.virtual.currency.bitcoin.contract.OmniContract;
 import live.lingting.virtual.currency.core.Contract;
@@ -33,26 +32,6 @@ import live.lingting.virtual.currency.tronscan.contract.TronscanContract;
 @RequiredArgsConstructor
 public class VirtualValidThread extends AbstractThread<Pay> {
 
-	/**
-	 * 未获取到支付状态超时时间, 单位: 分钟
-	 */
-	private static final Long EMPTY_TIMEOUT = TimeUnit.HOURS.toMinutes(6);
-
-	/**
-	 * 重试时长, 失败允许重试的时长, 单位: 分钟
-	 */
-	private static final Long RETRY_TIME = TimeUnit.HOURS.toMinutes(2);
-
-	/**
-	 * 失败延时, 多少分组内失败的订单不算失败. 单位: 分组
-	 */
-	private static final Long FAIL_TIME = TimeUnit.MINUTES.toMinutes(10);
-
-	/**
-	 * 成功的支付, 如果支付时间比支付信息创建时间早 SUCCESS_TIMEOUT, 表示支付失败
-	 */
-	private static final Long SUCCESS_TIMEOUT = TimeUnit.HOURS.toMinutes(12);
-
 	private final PayService service;
 
 	private final VirtualManager manager;
@@ -61,27 +40,13 @@ public class VirtualValidThread extends AbstractThread<Pay> {
 
 	private final ApiProperties properties;
 
+	private final ApiConfig config;
+
 	private final LambdaQueryWrapper<Pay> wrapper = Wrappers.<Pay>lambdaQuery()
 			// 限制 hash 不为空
 			.ne(Pay::getThirdPartTradeNo, "")
 			// 状态为等待
 			.eq(Pay::getStatus, PayStatus.WAIT);
-
-	public static Long getEmptyTimeout() {
-		if (!SpringUtils.isProd()) {
-			// 测试服 2分钟
-			return TimeUnit.MINUTES.toMinutes(1);
-		}
-		return EMPTY_TIMEOUT;
-	}
-
-	public static Long getRetryTime() {
-		if (!SpringUtils.isProd()) {
-			// 测试服1分钟
-			return TimeUnit.MINUTES.toMinutes(1);
-		}
-		return RETRY_TIME;
-	}
 
 	@Override
 	public List<Pay> listData() {
@@ -105,7 +70,7 @@ public class VirtualValidThread extends AbstractThread<Pay> {
 		// 没有获取到信息
 		if (!optional.isPresent()) {
 			// 获取允许的超时时间
-			final Long timeout = getEmptyTimeout();
+			final Long timeout = config.getEmptyInfoTimeout();
 			// 超时
 			if (minutes >= timeout) {
 				fail(pay, timeout + "分钟内未获取到交易信息!");
@@ -115,7 +80,7 @@ public class VirtualValidThread extends AbstractThread<Pay> {
 
 		final TransactionInfo info = optional.get();
 		// 失败, 且创建时长超过 FAIL_TIME
-		if (TransactionStatus.FAIL.equals(info.getStatus()) && minutes >= FAIL_TIME) {
+		if (TransactionStatus.FAIL.equals(info.getStatus()) && minutes >= config.getFailTimeout()) {
 			fail(pay, "支付失败!");
 		}
 		// 成功交易
@@ -129,6 +94,7 @@ public class VirtualValidThread extends AbstractThread<Pay> {
 	 * @author lingting 2021-06-09 15:34
 	 */
 	private void successHandler(Pay pay, TransactionInfo info) {
+		Long timeout;
 		// 合约不匹配
 		if (!info.getContract().equals(getContract(pay))) {
 			fail(pay, "支付货币不匹配!");
@@ -138,8 +104,9 @@ public class VirtualValidThread extends AbstractThread<Pay> {
 			fail(pay, "收款账户不匹配!");
 		}
 		// 支付时间比支付信息创建时间早 SUCCESS_TIMEOUT
-		else if (Duration.between(info.getTime(), pay.getCreateTime()).toMinutes() >= SUCCESS_TIMEOUT) {
-			fail(pay, "支付时间比支付信息创建时间早" + SUCCESS_TIMEOUT + "分钟以上");
+		else if (Duration.between(info.getTime(), pay.getCreateTime())
+				.toMinutes() >= (timeout = config.getInfoCreateTimeout())) {
+			fail(pay, "支付时间比支付信息创建时间早" + timeout + "分钟以上");
 		}
 		else {
 			success(pay, info);
@@ -168,7 +135,7 @@ public class VirtualValidThread extends AbstractThread<Pay> {
 	}
 
 	private void fail(Pay pay, String desc) {
-		manager.fail(pay, desc, LocalDateTime.now().plusMinutes(getRetryTime()));
+		manager.fail(pay, desc, LocalDateTime.now().plusMinutes(config.getRetryTimeout()));
 	}
 
 }
---
+++ payment-biz/src/main/java/live/lingting/ApiConfig.java
@@ -0,0 +1,81 @@
+package live.lingting;
+
+import static live.lingting.constant.SystemConfigConstants.PAY_EMPTY_INFO_TIMEOUT;
+import static live.lingting.constant.SystemConfigConstants.PAY_EMPTY_INFO_TIMEOUT_DEFAULT;
+import static live.lingting.constant.SystemConfigConstants.PAY_FAIL_TIMEOUT;
+import static live.lingting.constant.SystemConfigConstants.PAY_FAIL_TIMEOUT_DEFAULT;
+import static live.lingting.constant.SystemConfigConstants.PAY_INFO_CREATE_TIMEOUT;
+import static live.lingting.constant.SystemConfigConstants.PAY_INFO_CREATE_TIMEOUT_DEFAULT;
+import static live.lingting.constant.SystemConfigConstants.PAY_RETRY_TIMEOUT;
+import static live.lingting.constant.SystemConfigConstants.PAY_RETRY_TIMEOUT_DEFAULT;
+import static live.lingting.constant.SystemConfigConstants.VIRTUAL_SUBMIT_HASH_TIMEOUT;
+import static live.lingting.constant.SystemConfigConstants.VIRTUAL_SUBMIT_HASH_TIMEOUT_DEFAULT;
+
+import cn.hutool.core.convert.Convert;
+import com.hccake.ballcat.system.service.SysConfigService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import live.lingting.constant.SystemConfigConstants;
+
+/**
+ * @author lingting 2021/6/18 9:39
+ */
+@Component
+@RequiredArgsConstructor
+public class ApiConfig {
+
+	private final SysConfigService service;
+
+	/**
+	 * 虚拟货币提交Hash超时时间, 单位: 分钟
+	 * @author lingting 2021-06-18 09:47
+	 */
+	public Long getVirtualSubmitTimeout() {
+		return getByKey(VIRTUAL_SUBMIT_HASH_TIMEOUT, VIRTUAL_SUBMIT_HASH_TIMEOUT_DEFAULT);
+	}
+
+	/**
+	 * 未获取到交易信息等待时长。单位: 分钟
+	 * @author lingting 2021-06-18 09:54
+	 */
+	public Long getEmptyInfoTimeout() {
+		return getByKey(PAY_EMPTY_INFO_TIMEOUT, PAY_EMPTY_INFO_TIMEOUT_DEFAULT);
+	}
+
+	/**
+	 * 失败延时。单位: 分钟
+	 * @author lingting 2021-06-18 09:54
+	 */
+	public Long getFailTimeout() {
+		return getByKey(PAY_FAIL_TIMEOUT, PAY_FAIL_TIMEOUT_DEFAULT);
+	}
+
+	/**
+	 * 交易信息创建时间最大限制。单位: 分钟
+	 * @author lingting 2021-06-18 09:54
+	 */
+	public Long getInfoCreateTimeout() {
+		return getByKey(PAY_INFO_CREATE_TIMEOUT, PAY_INFO_CREATE_TIMEOUT_DEFAULT);
+	}
+
+	/**
+	 * 支付重试时长。单位: 分钟
+	 * @author lingting 2021-06-18 09:54
+	 */
+	public Long getRetryTimeout() {
+		return getByKey(PAY_RETRY_TIMEOUT, PAY_RETRY_TIMEOUT_DEFAULT);
+	}
+
+	private Long getByKey(String key, Long defaultVal) {
+		final String value = service.getConfValueByKey(key);
+		Long v;
+
+		if (!StringUtils.hasText(value) || (v = Convert.toLong(value)) < SystemConfigConstants.MIN) {
+			return defaultVal;
+		}
+
+		return v;
+	}
+
+}
