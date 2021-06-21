1163ff783f657b446d56f854385d6e94471754e4
聆听 <sunlisten.gzm@gmail.com>
2021-06-21 17:17:16 +0800

    :art: 重构汇率查询实现

--- payment-biz/src/main/java/live/lingting/rate/Rate.java
+++ payment-biz/src/main/java/live/lingting/rate/Rate.java
@@ -1,14 +1,21 @@
 package live.lingting.rate;
 
 import java.math.BigDecimal;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import live.lingting.sdk.enums.Currency;
 
 /**
  * @author lingting 2021/6/11 15:53
  */
-public interface Rate {
+@Component
+@RequiredArgsConstructor
+public class Rate {
 
-	BigDecimal CNY_RATE = BigDecimal.ONE;
+	public static final BigDecimal CNY_RATE = BigDecimal.ONE;
+
+	private final List<BaseRate> rates;
 
 	/**
 	 * 获取指定货币汇率
@@ -16,6 +23,17 @@ public interface Rate {
 	 * @return java.math.BigDecimal
 	 * @author lingting 2021-06-11 15:54
 	 */
-	BigDecimal get(Currency currency);
+	public BigDecimal get(Currency currency) {
+		if (currency == Currency.CNY) {
+			return CNY_RATE;
+		}
+		for (BaseRate rate : rates) {
+			final BigDecimal decimal = rate.get(currency);
+			if (decimal != null) {
+				return decimal;
+			}
+		}
+		return null;
+	}
 
 }
