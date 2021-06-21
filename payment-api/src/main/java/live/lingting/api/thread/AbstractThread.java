1188668b33f0dc46fe02230e5bff8481d94cc441
聆听 <sunlisten.gzm@gmail.com>
2021-06-21 19:02:22 +0800

    :zap: AbstractThread 线程异常处理

--- payment-api/src/main/java/live/lingting/api/thread/AbstractThread.java
+++ payment-api/src/main/java/live/lingting/api/thread/AbstractThread.java
@@ -1,8 +1,11 @@
 package live.lingting.api.thread;
 
 import cn.hutool.core.thread.ThreadUtil;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
@@ -12,14 +15,20 @@ public abstract class AbstractThread<E> extends Thread implements InitializingBe
 
 	@Override
 	public void run() {
+		final Logger log = LoggerFactory.getLogger(this.getClass());
 		while (!isInterrupted()) {
 			if (isContinue()) {
-				for (E e : listData()) {
-					if (isInterrupted() || !isContinue()) {
-						break;
-					}
-					handler(e);
+				List<E> data;
+
+				try {
+					data = listData();
 				}
+				catch (Exception de) {
+					log.error("获取数据出错!", de);
+					data = Collections.emptyList();
+				}
+
+				handlerList(data);
 			}
 
 			ThreadUtil.sleep(getSleepTime());
@@ -40,6 +49,25 @@ public abstract class AbstractThread<E> extends Thread implements InitializingBe
 	 * @author lingting 2021-06-10 17:25
 	 */
 	public abstract List<E> listData();
+
+	/**
+	 * 处理所有数据
+	 * @author lingting 2021-06-21 19:00
+	 */
+	protected void handlerList(List<E> data) {
+		final Logger log = LoggerFactory.getLogger(this.getClass());
+		for (E e : data) {
+			if (isInterrupted() || !isContinue()) {
+				break;
+			}
+			try {
+				handler(e);
+			}
+			catch (Exception he) {
+				log.error("处理数据出错!", he);
+			}
+		}
+	}
 
 	/**
 	 * 处理数据
