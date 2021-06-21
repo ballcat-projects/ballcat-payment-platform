006711eab26b0a4bcb0712b8eec98bfba83e2a68
聆听 <sunlisten.gzm@gmail.com>
2021-06-21 19:02:05 +0800

    :zap: 汇率相关代码优化

--- payment-biz/src/main/java/live/lingting/rate/BaseRate.java
+++ payment-biz/src/main/java/live/lingting/rate/BaseRate.java
@@ -1,12 +1,23 @@
 package live.lingting.rate;
 
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 import live.lingting.sdk.enums.Currency;
 
 /**
  * @author lingting 2021/6/21 17:07
  */
 public interface BaseRate {
+
+	/**
+	 * 请求连接超时, 5s
+	 */
+	Long CONNECT_TIMEOUT = TimeUnit.SECONDS.toMillis(5);
+
+	/**
+	 * 请求返回值读取超时, 10s
+	 */
+	Long READ_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
 
 	/**
 	 * 获取指定货币汇率
