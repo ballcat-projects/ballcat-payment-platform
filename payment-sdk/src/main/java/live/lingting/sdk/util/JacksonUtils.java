c5db31092db1d35b1db4d5c8873fd0b79b4ab78c
聆听 <sunlisten.gzm@gmail.com>
2021-06-25 16:35:34 +0800

    :bug: 修复jackson版本过低时, JacksonUtils 静态代码块执行异常

--- payment-sdk/src/main/java/live/lingting/sdk/util/JacksonUtils.java
+++ payment-sdk/src/main/java/live/lingting/sdk/util/JacksonUtils.java
@@ -3,25 +3,31 @@ package live.lingting.sdk.util;
 import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hccake.ballcat.common.util.ClassUtils;
 import com.hccake.ballcat.common.util.json.TypeReference;
 import java.lang.reflect.Type;
 import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
 import live.lingting.sdk.jackson.JavaTimeModule;
 
 /**
  * @author lingting 2021/6/9 14:28
  */
+@UtilityClass
 public class JacksonUtils {
 
 	static final ObjectMapper MAPPER = new ObjectMapper();
+	static final String JSON_READ_FEATURE_CLASS = "com.fasterxml.jackson.core.json.JsonReadFeature";
 
 	static {
 		// 序列化时忽略未知属性
 		MAPPER.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 		// 时间解析器
 		MAPPER.registerModule(new JavaTimeModule());
-		// 有特殊需要转义字符, 不报错
-		MAPPER.enable(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature());
+		if (ClassUtils.isPresent(JSON_READ_FEATURE_CLASS, JacksonUtils.class.getClassLoader())) {
+			// 有特殊需要转义字符, 不报错
+			MAPPER.enable(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature());
+		}
 	}
 
 	@SneakyThrows
