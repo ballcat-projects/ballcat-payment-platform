b1b0ef131cb3a2c21e42d2aa17264e767f8f5626
聆听 <sunlisten.gzm@gmail.com>
2021-08-19 16:11:08 +0800

    :zap: 关闭支付宝支付的异常日志

--- payment-biz/src/main/java/live/lingting/payment/biz/real/third/AliManager.java
+++ payment-biz/src/main/java/live/lingting/payment/biz/real/third/AliManager.java
@@ -1,5 +1,6 @@
 package live.lingting.payment.biz.real.third;
 
+import com.alipay.api.internal.util.AlipayLogger;
 import org.springframework.stereotype.Component;
 import live.lingting.payment.ali.AliPay;
 import live.lingting.payment.ali.constants.AliPayConstant;
@@ -11,6 +12,13 @@ import live.lingting.payment.sdk.enums.ThirdPart;
  */
 @Component
 public class AliManager extends AbstractThirdManager<AliPay> {
+
+	@Override
+	public void init() {
+		super.init();
+		// 关闭异常日志
+		AlipayLogger.setNeedEnableLogger(false);
+	}
 
 	@Override
 	public ThirdPart getThird() {
